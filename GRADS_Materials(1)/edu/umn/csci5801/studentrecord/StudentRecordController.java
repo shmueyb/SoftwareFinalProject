70d09a7f9eba1c0c6d700535d8f6933fe5c2ee52
cjreeves <reeve098@umn.edu>
2013-12-10 19:38:30 -0600

    Attempt at start of progress summary function

--- GRADS_Materials(1)/edu/umn/csci5801/studentrecord/StudentRecordController.java
+++ GRADS_Materials(1)/edu/umn/csci5801/studentrecord/StudentRecordController.java
@@ -3,6 +3,8 @@ package edu.umn.csci5801.studentrecord;
 import edu.umn.csci5801.db.DatabaseAccessException;
 import edu.umn.csci5801.db.StudentDAO;
 import edu.umn.csci5801.studentrecord.program.Department;
+import edu.umn.csci5801.studentrecord.requirements.MilestoneSet;
+import edu.umn.csci5801.studentrecord.requirements.RequirementCheckResult;
 import edu.umn.csci5801.studentrecord.transcript.CourseTaken;
 import edu.umn.csci5801.studentrecord.transcript.ProgressSummary;
 
@@ -34,7 +36,7 @@ public class StudentRecordController {
      * @return
      * @throws DatabaseAccessException
      */
-    public static ProgressSummary generateProgressSummary(String studentID) throws DatabaseAccessException{
+    public static ProgressSummary generateProgressSummary(String studentID) throws DatabaseAccessException, FileNotFoundException {
         return simulateCourses(studentID, new ArrayList<CourseTaken>());
     }
 
@@ -46,10 +48,49 @@ public class StudentRecordController {
      * @throws DatabaseAccessException
      */
     public static ProgressSummary simulateCourses(String studentID,
-                                                  List<CourseTaken> courses)  throws DatabaseAccessException{
+                                                  List<CourseTaken> courses) throws DatabaseAccessException, FileNotFoundException {
         ProgressSummary summary = new ProgressSummary();
 
         //TODO: fill in
+        StudentRecord record = StudentDAO.getStudentByID(studentID);
+        List<RequirementCheckResult> requirementCheckResultsList = null;
+        courses.addAll(record.getCoursesTaken());
+
+        List <MilestoneSet> milestonesSet = record.getMilestonesSet();
+        summary.setStudent(record.getStudent());
+        summary.setDegreeSought(record.getDegreeSought());
+        summary.setAdvisors(record.getAdvisors());
+        summary.setCommittee(record.getCommittee());
+        summary.setNotes(record.getNotes());
+        summary.setDepartment(record.getDepartment());
+        summary.setTermBegan(record.getTermBegan());
+        boolean passed = true;
+        double totalGradePoints=0;
+        double totalCredits =0;
+        for(CourseTaken course : courses){
+            String courseName = course.getCourse().getName();
+
+            if(!(course.getGrade().equals("S"))||(course.getGrade().equals("N"))){
+
+                double grade = course.getGrade().numericValue();
+                double credits = Double.parseDouble(course.getCourse().getNumCredits());
+
+                totalGradePoints+= grade;
+                totalCredits+= credits;
+
+                if(grade< 1.667){
+
+                    passed = false;
+                }
+
+            }
+            RequirementCheckResult requirementCheckResult = new RequirementCheckResult(courseName, passed);
+            requirementCheckResultsList.add(requirementCheckResult );
+        }
+
+        double totalGPA = totalGradePoints/totalCredits;
+
+        summary.setRequirementCheckResults(requirementCheckResultsList);
 
         return null;
     }

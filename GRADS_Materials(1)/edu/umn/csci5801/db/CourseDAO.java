296eaa848766a89efdfb18c223b837ac92176e01
Sam Blustin <blustinsamahs@gmail.com>
2013-12-12 23:22:32 -0600

    implemented CourseDao

--- GRADS_Materials(1)/edu/umn/csci5801/db/CourseDAO.java
+++ GRADS_Materials(1)/edu/umn/csci5801/db/CourseDAO.java
@@ -1,5 +1,10 @@
 package edu.umn.csci5801.db;
 
+import edu.umn.csci5801.session.Users;
+import edu.umn.csci5801.studentrecord.transcript.Course;
+
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Ben
@@ -8,5 +13,18 @@ package edu.umn.csci5801.db;
  * To change this template use File | Settings | File Templates.
  */
 public class CourseDAO {
+    public static Course getCourseByID(String courseID) throws DatabaseAccessException{
+        List<Course> courseList=null;
 
+        courseList= FileAccess.getInstance().getCourseJSON();
+
+        Course currentCourse=null;
+        for (Course course: courseList){
+            if (courseID.equals(course.getId())){
+                currentCourse=course;
+            }
+        }
+
+        return currentCourse;
+    }
 }

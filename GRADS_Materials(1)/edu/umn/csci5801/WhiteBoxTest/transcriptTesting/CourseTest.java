7467e0703746f1f069551aea91b9f361db7282ed
cjreeves <reeve098@umn.edu>
2013-12-17 20:53:41 -0600

    Adding CourseTest

---
+++ GRADS_Materials(1)/edu/umn/csci5801/WhiteBoxTest/transcriptTesting/CourseTest.java
@@ -0,0 +1,11 @@
+package edu.umn.csci5801.WhiteBoxTest.transcriptTesting;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lil4000
+ * Date: 12/17/13
+ * Time: 8:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CourseTest {
+}
